b108f9d18dc7c79dd7d6a6c2f12a1f2d39b5f174
Luciano Porto <luciano.bapo@gmail.com>
2016-12-19 01:03:29 -0200

    automatic-commit-alias

--- database/migrations/2016_12_19_100000_create_pages_table.php
+++ database/migrations/2016_12_19_100000_create_pages_table.php
@@ -31,41 +31,16 @@ class CreatePagesTable extends BaseMigration {
             /**
              * Mandante do Banco de dados
              */
-//            $table->string('mandante')->index();
+            $table->string('mandante')->index();
 
             // Campos
 //            $table->timestamp('start_date')->default(DB::raw('CURRENT_TIMESTAMP'))->index();
 //            $table->timestamp('end_date')->default(DB::raw('CURRENT_TIMESTAMP'))->index();
 
             $table->string('nome')->nullable();
-//            $table->string('description')->nullable();
-//            $table->string('paramProfileImageSize')->nullable();
-//            $table->string('paramProfileImagePosition')->nullable();
-//            $table->string('paramProfileImageX')->nullable();
-//            $table->string('paramProfileImageY')->nullable();
-//            $table->string('paramName')->nullable();
-//            $table->string('paramFirstName')->nullable();
-//            $table->string('paramNameSize')->nullable();
-//            $table->string('paramNameColor')->nullable();
-//            $table->string('paramNameX')->nullable();
-//            $table->string('paramNameY')->nullable();
-//            $table->string('titleSlug')->index();
-//            $table->string('file')->nullable();
-//            $table->string('file1')->nullable();
-//            $table->string('file2')->nullable();
-//            $table->string('file3')->nullable();
-//            $table->string('file4')->nullable();
-//            $table->string('file5')->nullable();
-//            $table->string('file6')->nullable();
-//            $table->string('file7')->nullable();
-//            $table->string('file8')->nullable();
-//            $table->string('file9')->nullable();
-//            $table->string('file10')->nullable();
-//            $table->string('file11')->nullable();
-//            $table->string('file12')->nullable();
-//            $table->string('file13')->nullable();
-//            $table->string('file14')->nullable();
-//            $table->string('file15')->nullable();
+            $table->string('titulo')->nullable();
+            $table->string('descricao')->nullable();
+            $table->string('conteudo')->nullable();
         });
     }
 
