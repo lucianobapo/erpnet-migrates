ff9c42b8022c4131d6d393dffe57fcdbedc052fb
Luciano Porto <luciano.bapo@gmail.com>
2017-05-09 15:35:28 -0300

    automatic-commit-alias

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # erpnet-migrates
 
+## Start using
+```shell
+php artisan vendor:publish --tag=erpnetMigratesConfig
+php artisan vendor:publish --tag=erpnetMigratesMigrations
+php artisan migrate
+```
+
 [Site do GitHub](https://github.com/lucianobapo/erpnet-migrates)
 
 [![Latest Stable Version](https://poser.pugx.org/ilhanet/erpnet-migrates/v/stable)](https://packagist.org/packages/ilhanet/erpnet-migrates) 
--- config/erpnetMigrates.php
+++ config/erpnetMigrates.php
@@ -1,4 +1,20 @@
 <?php
 return [
-    'tables' => [],
+    'tables' => [
+        /*
+         * Config example
+         *
+         * 'table_name' => [
+         *  'bindInterface' => 'ExampleInterface',
+         *  'bindRepository' => 'ExampleRepository',
+         *  'routeController' => 'ExampleController',
+         *  'routeLinkName' => 'Example',
+         *  'routePrefix' => 'example',
+         *  'fields' => [
+         *      'field1',
+         *      'field2',
+         *   ],
+         * ],
+         */
+    ],
 ];
